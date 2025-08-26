9671ee03dac12516d70f8f09668cbc0c44e9fab2
jieunjieune <jieune120@gmail.com>
2025-08-26 18:23:10 +0900

    feat: web 설정

---
+++ mypaletteai-server/src/main/java/geniuseun/mypaletteai/config/WebConfig.java
@@ -0,0 +1,23 @@
+package geniuseun.mypaletteai.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                // React 개발서버 주소 허용
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:3000")
+                        .allowedMethods("*");
+            }
+        };
+    }
+}
