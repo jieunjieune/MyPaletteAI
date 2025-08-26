3aee564f9d1c31dbfecc118a886c0b03a30ef2bf
jieunjieune <jieune120@gmail.com>
2025-08-26 18:23:56 +0900

    feat: todayPalette 메인페이지 연결

--- mypaletteai-react/src/pages/Main.js
+++ mypaletteai-react/src/pages/Main.js
@@ -1,6 +1,15 @@
+import { useDispatch, useSelector } from 'react-redux';
 import MainCSS from './Main.module.css';
+import { useEffect } from 'react';
+import { callTodayPaletteApi } from '../apis/TodayPaletteAPICalls';
 
 function Main() {
+	const dispatch = useDispatch();
+	const todayPalette = useSelector((state) => state.todayPalette);
+
+	useEffect(() => {
+		dispatch(callTodayPaletteApi());
+	}, [dispatch]);
 	
 	return (
 		<div className={MainCSS.container}>
@@ -8,7 +17,22 @@ function Main() {
 				<img src="/images/main/mypaletteailogo.png" className={MainCSS.logo}/>
 			</div>
 			<div>
-				<a>오늘의 추천 Palette</a>
+				<h3>오늘의 추천 Palette</h3>
+				<h2>{todayPalette?.title}</h2>
+				<p>{todayPalette?.mood}</p>
+				<div style={{ display: "flex" }}>
+				{todayPalette?.recommendedColors?.map((color, index) => (
+					<div
+					key={index}
+					style={{
+						width: "60px",
+						height: "60px",
+						backgroundColor: color,
+						border: "1px solid #ccc",
+					}}
+					></div>
+				))}
+				</div>
 			</div>
 		</div>
 	)
