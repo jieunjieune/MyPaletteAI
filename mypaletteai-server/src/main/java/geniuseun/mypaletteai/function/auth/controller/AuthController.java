a812104e5f1b4bceaee772b2e085e12df193536e
jieunjieune <jieune120@gmail.com>
2025-08-28 23:51:23 +0900

    fix: 불필요한 주석 삭제

--- mypaletteai-server/src/main/java/geniuseun/mypaletteai/function/auth/controller/AuthController.java
+++ mypaletteai-server/src/main/java/geniuseun/mypaletteai/function/auth/controller/AuthController.java
@@ -21,21 +21,6 @@ public class AuthController {
     private final TokenProvider tokenProvider;
     private final AuthService authService;
 
-//    @PostMapping("/login")
-//    public TokenDTO login(@RequestParam Long userId) {
-//        String accessToken = tokenProvider.createAccessToken(userId);
-//        String refreshToken = tokenProvider.createRefreshToken(userId);
-//
-//        return TokenDTO.builder()
-//                .tokenType("Bearer")
-//                .userId(userId)
-//                .accessToken(accessToken)
-//                .refreshToken(refreshToken)
-//                .accessTokenExpiresIn(3600000L)
-//                .refreshTokenExpiresIn(1209600000L)
-//                .build();
-//    }
-
     @PostMapping("/login")
     public TokenDTO login(@RequestBody LoginRequest request) {
         return authService.login(request);
