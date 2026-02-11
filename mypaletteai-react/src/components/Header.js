0735c485772c8be4e11eb142ecf6602e2b780ee1
jieunjieune <jieune120@gmail.com>
2026-02-12 01:26:22 +0900

    로고명 수정

--- mypaletteai-react/src/components/Header.js
+++ mypaletteai-react/src/components/Header.js
@@ -31,7 +31,7 @@ function Header() {
 		<div className={`${HeaderCSS.box} flex items-center justify-between`}>
 			{/* 로고 */}
 			<Link to="/" className={HeaderCSS.headerLogo}>
-			<img src={process.env.PUBLIC_URL + "/images/main/MyPaletteAiLogo.png"}
+			<img src={process.env.PUBLIC_URL + "/images/main/mypaletteailogo.png"}
 				alt="로고"
 				className={HeaderCSS.logoImg}
 			/>
