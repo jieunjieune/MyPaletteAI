0ddd9fdeed6f0bcf29f304a9e1b47961f4a83646
jieunjieune <jieune120@gmail.com>
2026-03-29 19:40:59 +0900

    상태체크 허용

--- mypaletteai-server/src/main/java/geniuseun/mypaletteai/config/SecurityConfig.java
+++ mypaletteai-server/src/main/java/geniuseun/mypaletteai/config/SecurityConfig.java
@@ -35,6 +35,7 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .requestMatchers("/",
                                 "/auth/**",
+                                "/health", 
                                 "/user/**",
                                 "/palettes/**",
                                 "/make/**",
